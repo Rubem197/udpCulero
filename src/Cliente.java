5076a5ece8b4c96958495cf5219ebfcd7b32fd86
Rubem197 <ruben.lindes@iesnervion.es>
2023-01-31 14:28:36 +0100

    awa

---
+++ src/Cliente.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.net.*;
+
+public class Cliente {
+    public static void main(String[] args) {
+        try {
+            InetAddress direccion = InetAddress.getLocalHost();
+            DatagramSocket socket = new DatagramSocket();
+
+            String mensaje = "diego";
+            byte[] buffer = mensaje.getBytes();
+
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, direccion, 42500);
+            socket.send(packet);
+            byte[] bufferEntrada = new byte[64];
+            DatagramPacket packetRecibir = new DatagramPacket(bufferEntrada, bufferEntrada.length);
+            socket.receive(packetRecibir);
+            String mensajeRecibido =  new String(packetRecibir.getData());
+            System.out.println(mensajeRecibido.trim());
+
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            DatagramSocket socket = new DatagramSocket(42500);
+            byte[] buffer = new byte[64];
+
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            socket.receive(packet);
+
+            String mensaje = "Hola "+ new String(packet.getData());
+
+            byte[] bufferSalida = mensaje.getBytes();
+
+            DatagramPacket packetSalida = new DatagramPacket(bufferSalida, bufferSalida.length, packet.getAddress(), packet.getPort());
+
+            socket.send(packetSalida);
+
+
+        }catch (SocketException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
